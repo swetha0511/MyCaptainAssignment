622d061aab0f6848e3e8fa59fbbde54941243350
swetha0511 <100515321+swetha0511@users.noreply.github.com>
2022-02-28 18:33:38 +0530

    Add files via upload

---
+++ circlefile.py
@@ -0,0 +1,19 @@
+#Radiusofcircle
+
+r = float(input("Input the radius of the circle: "))
+
+area = 3.1416 * r
+
+print("Radius:", r)
+
+print("Area = ", area)
+
+
+#FileExtension
+
+filename = input("Input the Filename: ")
+
+extension = filename.split(".")
+
+print ("The extension of the file is : ", extension[1]))
+
